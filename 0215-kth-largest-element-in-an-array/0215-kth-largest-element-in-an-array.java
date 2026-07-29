f931f7c614475280f7e93be37f4a9142abde815f
Prince <prince.x3434@gmail.com>
2026-07-29 13:58:21 +0530

    Time: 70 ms (44.82%), Space: 74.2 MB (61.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         PriorityQueue <Integer> pq = new PriorityQueue();
         for(int i=0;i<nums.length;i++){
             pq.add(nums[i]);
-            if(pq.size()==k+1) pq.remove();
+            if(pq.size()>k) pq.remove();
         }
       return pq.peek();
 }
