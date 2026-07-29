9ad8770a8dee077a318bbc31d9e9e2ce6c979790
Prince <prince.x3434@gmail.com>
2026-07-29 19:02:23 +0530

    Time: 10 ms (27.35%), Space: 47.4 MB (6.84%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-    for(int i=0;i<nums.length;i++){
-        ans = ans^nums[i];
-        
-    }
-    return ans;
-        
-    }
+     Arrays.sort(nums);
+     for(int i=0;i<nums.length-1;i=i+2){
+        if((nums[i]^nums[i+1])!=0) return nums[i];
+     }
+     return nums[nums.length-1];
+}
 }
